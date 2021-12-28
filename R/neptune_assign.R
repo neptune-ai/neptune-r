599c544354280f4ae241c9964cc063eaf645683e
Mateusz Dominiak <mateusz.dominiak@adcolony.com>
2021-12-28 18:10:26 +0100

    add some run lvl methods

--- R/neptune_assign.R
+++ R/neptune_assign.R
@@ -1,5 +1,5 @@
 neptune_assign <-
 function(x, value, wait=FALSE) {
-  check_handler(x)
+  check_handler_or_run(x)
   x$assign(value=value, wait=wait)
 }
--- R/neptune_fetch.R
+++ R/neptune_fetch.R
@@ -1,5 +1,5 @@
 neptune_fetch <-
 function(x) {
-  check_handler(x)
+  check_handler_or_run(x)
   x$fetch()
 }
