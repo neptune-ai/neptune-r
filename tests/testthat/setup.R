a75ea0a1b9b3363263b7dede192ae5d1c446275e
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2023-02-06 10:57:19 +0100

    Fix CI

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,6 +1,10 @@
 tryCatch(
     neptune_init(),
     error = function(e) {
+        install.packages("reticulate")
+        library(reticulate)
+        install_miniconda()
+
         neptune_install()
     }
 )
