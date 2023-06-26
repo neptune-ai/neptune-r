da28ccbf57832aee19ef25702c4ca9d6fa4fc34e
ArcadeAntics <nsnjsnd@gmail.com>
2023-06-25 20:43:11 -0400

    Update neptune_init_run.R
    Fixes for upcoming major update **this.path_2.0.0**

--- R/neptune_init_run.R
+++ R/neptune_init_run.R
@@ -21,8 +21,15 @@ neptune_init_run <-
     if (!(mode %in% c("async", "sync", "offline", "debug", "read-only"))) {
       stop('mode must be one of "async", "sync", "offline", "debug", "read-only"')
     }
-    if (is.null(source_files) & !interactive()) {
-      source_files <- c(this.path::this.path())
+    if (is.null(source_files)) {
+      this.path_version <- as.numeric_version(getNamespaceVersion("this.path"))
+      source_files <- if (this.path_version >= "2.0.0")
+        this.path::try.sys.path()
+      else if (this.path_version >= "1.3.0")
+        this.path::try.this.path()
+      else tryCatch({
+        this.path::this.path()
+      }, error = function(e) NA_character_)
     }
     requireNamespace("reticulate", quietly = TRUE)
     if (!isNamespaceLoaded("reticulate")) {
